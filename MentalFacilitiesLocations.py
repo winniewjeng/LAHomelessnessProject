d88b9c93f5f077e077bc3c953d8aa6eba36f18d4
winniewjeng <winniewjeng@gmail.com>
2019-04-06 00:26:31 -0700

    extraction finished. organize geolocations

--- MentalFacilitiesLocations.py
+++ MentalFacilitiesLocations.py
@@ -1,29 +1,73 @@
 # importing the requests library
 import requests
 import os
+import csv
 import pprint
+import time
 
-# api-endpoint
-URL = "https://maps.googleapis.com/maps/api/geocode/json?address="
-# location given here
-location = "Apple Pasadena"
-api = os.environ.get('GOOGLE_API_KEY')
-URL = URL + location + "&key=" + api
+addresses = []
+f = open('Mental_Health_Service_Providers_2016.csv')
+csv_f = csv.reader(f)
 
-# sending get request and saving the response as response object
-r = requests.get(url=URL)
+for row in csv_f:
+    address = row[2] + ', ' + row[3]
+    addresses.append(address)
+    # print(row[2]+' '+row[3])
 
-# extracting data in json format
-data = r.json()
 
-# print the data in terminal
-pprint.pprint(data)
+# dictionary of address and lat and long
+geoLocations = {}
+lat = []
+lon = []
 
-# extracting latitude, longitude and formatted address
-# of the first matching location
-latitude = data['results'][0]['geometry']['location']['lat']
-longitude = data['results'][0]['geometry']['location']['lng']
-formatted_address = data['results'][0]['formatted_address']
+del addresses[0]
 
-# printing the output
-print("Latitude:%s\nLongitude:%s\nFormatted Address:%s" % (latitude, longitude, formatted_address))
+
+for location in addresses:
+    # print(location + '\n')
+    # api-endpoint
+    URL = "https://maps.googleapis.com/maps/api/geocode/json?address="
+    # api key
+    api = os.environ.get('GOOGLE_API_KEY')
+    # # building the url
+    URL = URL + location + "&key=" + api
+    # sending get request and saving the response as response object
+    r = requests.get(url=URL)
+    # extracting data in json format
+    data = r.json()
+    # pprint.pprint(data)  # print the data in terminal
+    # extracting latitude, longitude and formatted address of the first matching location
+    formatted_address = data['results'][0]['formatted_address']
+    latitude = data['results'][0]['geometry']['location']['lat']
+    longitude = data['results'][0]['geometry']['location']['lng']
+    # printing the output
+    print("Latitude:%s\nLongitude:%s\nFormatted Address:%s" % (latitude, longitude, formatted_address))
+    # storing things inside the geoLocations dictionary
+
+    # make the request with exponential backoff:
+    time.sleep(0.1)
+
+
+# #
+# location = addresses[33]
+# print(location + '\n')
+# # building the url
+# URL = URL + location + "&key=" + api
+# # sending get request and saving the response as response object
+# r = requests.get(url=URL)
+# # extracting data in json format
+# data = r.json()
+# pprint.pprint(data)  # print the data in terminal
+# # extracting latitude, longitude and formatted address of the first matching location
+# formatted_address = data['results'][0]['formatted_address']
+# latitude = data['results'][0]['geometry']['location']['lat']
+# longitude = data['results'][0]['geometry']['location']['lng']
+# # printing the output
+# print("Latitude:%s\nLongitude:%s\nFormatted Address:%s" % (latitude, longitude, formatted_address))
+# # storing things inside the geoLocations dictionary
+#
+#
+#
+
+
+
